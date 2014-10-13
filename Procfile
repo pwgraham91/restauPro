399d012aaa8bf1feb606ce186a073c213844a19c
Peter <pwgraham91@gmail.com>
2014-10-13 14:07:21 -0700

    heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn restaupro.wsgi --log-file -
--- restaupro/settings.py
+++ restaupro/settings.py
@@ -24,7 +24,6 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
 AUTH_USER_MODEL = 'host_management.Restaurant'
 LOGIN_REDIRECT_URL = 'profile'
 LOGIN_URL = 'login'
@@ -106,3 +105,15 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
